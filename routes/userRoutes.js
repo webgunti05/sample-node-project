159100eee03fb7128d3b5132b659fef4b7320910
anjaneyulu <anjaneyulu.gunti@indegene.com>
2023-02-03 16:05:36 +0530

    removing empty space

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 import { getUsers, createUser, deleteUserById, getUserById,updateUser, loginUser } from '../controllers/userController.js';
 
 router.get('/get-users', getUsers);
-router.get('/get-user-details/:id',  getUserById);
+router.get('/get-user-details/:id', getUserById);
 router.post('/create-user', createUser);
 router.patch('/update-user/:id', updateUser)
 router.delete('/delete-user/:id', deleteUserById);
